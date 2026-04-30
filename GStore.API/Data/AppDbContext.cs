075c5a464573c9af93313ad41d50adc739b640f4
GalloJunior <gallojunior@gmail.com>
2026-04-30 11:08:04 -0300

    Criada a camada de dados e o contexto

---
+++ GStore.API/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using System.Runtime.Intrinsics.X86;
+using GStore.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GStore.API.Data;
+
+public class AppDbContext : IdentityDbContext<Usuario>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+}
